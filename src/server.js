78ca46edf2cd6c39f96b508576a8bc97fe851209
Aijuyy26 <alexandralevchuk6@gmail.com>
2024-07-13 17:53:57 +0300

    ggg

--- src/server.js
+++ src/server.js
@@ -1,10 +1,8 @@
 import express from 'express';
 import pino from 'pino-http';
 import cors from 'cors';
-
 import { env } from './utils/env.js';
 import { getAllContacts, getContactById } from '../src/services/contacts.js';
-
 
 const PORT = Number(env('PORT', '3000'));
 
